7f6cd996dfcf7cf6c1285e46cd4f450c3939289f
Alexandre-Luft-Duarte <alexandreduarte43234@gmail.com>
2025-04-11 11:17:24 -0300

    cliques em débito aberto correto

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from utils import ler_codigos_csv
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 
 # Inicializa o Chrome
 driver = webdriver.Chrome()
@@ -15,19 +16,21 @@ driver.get("https://www.saomiguel.sc.gov.br/")
 time.sleep(3)
 
 # Clica na barra de pesquisa
-botao_servicos = driver.find_element(By.CLASS_NAME, "form-control")
+botao_servicos = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "form-control")))
 botao_servicos.click()
 
 # Pesquisa iptu
 botao_servicos.send_keys('IPTU')
 
 # Clica no botão de pesquisar
-botao_pesquisar_iptu = driver.find_element(By.CLASS_NAME, 'icon-smo-search').click()
+botao_pesquisar_iptu = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, 'icon-smo-search')))
+botao_pesquisar_iptu.click()
 
-time.sleep(1)
+time.sleep(3)
 
 # Clica em iptu
-botao_iptu = driver.find_element(By.XPATH, "/html/body/main/div/div[2]/div/a[1]").click()
+botao_iptu = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, "/html/body/main/div/div[2]/div/a[1]")))
+botao_iptu.click()
 
 time.sleep(3)
 
@@ -38,32 +41,36 @@ driver.switch_to.window(abas[1])
 # Abre e Começa a navegar no site do betha
 driver.get('https://e-gov.betha.com.br/cdweb/03114-473/contribuinte/rel_guiaiptu.faces')
 
-time.sleep(2)
+time.sleep(3)
 
 # Inserindo os códigos dos imóveis
-cont = 0
 for cont, codigo in enumerate(ler_codigos_csv(), 1):
     print(f"Pesquisando o código do imóvel {codigo}")
 
     if cont == 1:
         # Clica onde após o clique aparece o campo para colocar o código
-        botao_codigo_imovel = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div/div/form/div[1]/div[1]/div[2]/div/div[1]/div[3]/div/div/div/a")
+        botao_codigo_imovel = WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div/div/div/form/div[1]/div[1]/div[2]/div/div[1]/div[3]/div/div/div/a")))
         botao_codigo_imovel.click()
 
         # Clica no campo para escrever e escreve o código
-        campo_input_imovel = driver.find_element(By.ID, "mainForm:iImoveis")
-        campo_input_imovel.click()
+        campo_input_imovel = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, "mainForm:iImoveis")))
         campo_input_imovel.clear()
         campo_input_imovel.send_keys(str(codigo))
         
-        time.sleep(1)
-
         # Fazendo a pesquisa
-        botao_continuar_pesquisar_imóvel = driver.find_element(By.ID, "mainForm:btIImoveis")
+        botao_continuar_pesquisar_imóvel = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, "mainForm:btIImoveis")))
         botao_continuar_pesquisar_imóvel.click()
-        time.sleep(1)
+
+        # Marcando a opção de débitos em aberto
+        botao_debitos_em_aberto = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, "P0")))
+        botao_debitos_em_aberto.click()
+
+        # Marcando a opção de marcar todas as parcelas
+        botato_marcar_todas_parcelas = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, "selectAll")))
+        botato_marcar_todas_parcelas.click()
     else:
-        # Após fazer a consulta do iptu, clica em fazer nova consulta
+        # Após fazer a consulta do primeiro iptu, clica em fazer nova consulta
         botao_nova_consulta = WebDriverWait(driver, 30).until(
         EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div/div/div/form/div[1]/div[1]/div[3]/span/input")))
         botao_nova_consulta.click()
@@ -76,8 +83,17 @@ for cont, codigo in enumerate(ler_codigos_csv(), 1):
         campo_input_imovel.send_keys(str(codigo))
 
         # Faz a pesquisa novamente
-        botao_continuar_pesquisar_imóvel = driver.find_element(By.ID, "mainForm:btIImoveis")
+        botao_continuar_pesquisar_imóvel = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, "mainForm:btIImoveis")))
         botao_continuar_pesquisar_imóvel.click()
+    for tentaivas in range(2):
+        try:
+            # Marcando a opção de débitos em aberto
+            botao_debitos_em_aberto2 = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, "P0")))
+            botao_debitos_em_aberto2.click()
+        except StaleElementReferenceException:
+            print('erro')
+            time.sleep(1)
+
 
 print("Deu boa")
 time.sleep(5)
--- utils.py
+++ utils.py
@@ -5,3 +5,5 @@ def ler_codigos_csv(caminho_csv="arquivo/iptu_96_25032025.csv"):
     df = pd.read_csv(caminho_csv, delimiter=";")
     codigos_imoveis = df['imovel_prefeitura'].tolist() # Converte as colunas dos códigos em uma lista
     return codigos_imoveis
+
+
