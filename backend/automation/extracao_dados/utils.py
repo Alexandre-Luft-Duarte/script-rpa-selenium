f1bd1ec3aa25dca642e11f6faf6f2961ef5b641c
Alexandre-Luft-Duarte <alexandreduarte43234@gmail.com>
2025-04-29 09:11:36 -0300

    ajuste do caminho dos pdfs na branch de teste

--- backend/automation/extracao_dados/utils.py
+++ backend/automation/extracao_dados/utils.py
@@ -4,7 +4,7 @@ import pdfplumber, os, re, logging
 logging.getLogger("pdfminer").setLevel(logging.ERROR)
 
 # Caminho absoluto da pasta pdfs_teste
-pasta = os.path.join(os.path.dirname(__file__), "/home/flexpro/Área de Trabalho/RPA/iptu_selenium/backend/emissao_pdfs/pdfs_iptu")
+pasta = os.path.join(os.path.dirname(__file__), "/home/flexpro/Documentos/RPA_Projetos/iptu_selenium/backend/automation/emissao_pdfs/pdfs_iptu")
 arquivos = os.listdir(pasta)
 
 def extrair_codigo():
